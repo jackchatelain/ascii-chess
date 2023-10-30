e2f2da1dbfaa1fe1447fe693ae33eed6171cd4d5
Ben Chatelain <ben@octop.ad>
2023-10-29 19:48:09 -0600

    ✨ Capture output and attempt to send input

--- ascii_chess.py
+++ ascii_chess.py
@@ -1,27 +1,43 @@
 import subprocess
+from subprocess import Popen
+from subprocess import PIPE
+from subprocess import STDOUT
 
-
-# os.system("lc0")
-
-# subprocess.run([""])
+from os import linesep
 
 # command = 'whoami'
 command = 'lc0'
 
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# output, error = process.communicate("isready\n")
+def parse_output(output):
+    print(f"<<{output}")
+    if output.endswith("2023"):
+        return "isready"
 
-while True:
-    stream = process.stderr
-    output = stream.readline().decode('utf-8')
-    if output == '' and process.poll() is not None:
-        break
-    if output:
-        print(output.strip())
+def send_input(input, stdin):
+    print(f">>{input}")
+    stdin.write(b"{input}\n")
+    stdin.flush()
 
-rc = process.poll()
-if rc == 0:
-    print("Command succeeded.")
-else:
-    error = process.stderr.read().decode()
-    print(f"Command failed ({rc}): {error}")
+def main():
+    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    output_stream = process.stderr
+    error_stream = process.stdout
+
+    while True:
+        output = output_stream.readline().decode('utf-8')
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            input = parse_output(output.strip())
+            if input is not None:
+                send_input(input, process.stdin)
+
+    rc = process.poll()
+    if rc == 0:
+        print("Command succeeded.")
+    else:
+        error = error_stream.read().decode()
+        print(f"Command failed ({rc}): {error}")
+
+main()
