da0aeaa881bd9fe278f4e71351205d42a7eb47f5
Ben Chatelain <ben@octop.ad>
2023-10-29 19:59:39 -0600

    ♻️ Use pexpect

--- ascii_chess.py
+++ ascii_chess.py
@@ -4,40 +4,49 @@ from subprocess import PIPE
 from subprocess import STDOUT
 
 from os import linesep
+import pexpect
 
 # command = 'whoami'
 command = 'lc0'
 
-def parse_output(output):
-    print(f"<<{output}")
-    if output.endswith("2023"):
-        return "isready"
+# def parse_output(output):
+#     print(f"<<{output}")
+#     if output.endswith("2023"):
+#         return "isready"
 
-def send_input(input, stdin):
-    print(f">>{input}")
-    stdin.write(b"{input}\n")
-    stdin.flush()
+# def send_input(input, stdin):
+#     print(f">>{input}")
+#     stdin.write(b"{input}\n")
+#     stdin.flush()
 
-def main():
-    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+# def main():
+#     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-    output_stream = process.stderr
-    error_stream = process.stdout
+#     output_stream = process.stderr
+#     error_stream = process.stdout
 
-    while True:
-        output = output_stream.readline().decode('utf-8')
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            input = parse_output(output.strip())
-            if input is not None:
-                send_input(input, process.stdin)
+#     while True:
+#         output = output_stream.readline().decode('utf-8')
+#         if output == '' and process.poll() is not None:
+#             break
+#         if output:
+#             input = parse_output(output.strip())
+#             if input is not None:
+#                 send_input(input, process.stdin)
 
-    rc = process.poll()
-    if rc == 0:
-        print("Command succeeded.")
-    else:
-        error = error_stream.read().decode()
-        print(f"Command failed ({rc}): {error}")
+#     rc = process.poll()
+#     if rc == 0:
+#         print("Command succeeded.")
+#     else:
+#         error = error_stream.read().decode()
+#         print(f"Command failed ({rc}): {error}")
 
-main()
+# main()
+
+
+child = pexpect.spawn(command)
+child.expect('.*Jul 22 2023', timeout=300)
+print(child.before.decode() + child.after.decode())
+child.sendline('isready')
+child.expect('.*readyok', timeout=300)
+print(child.before.decode() + child.after.decode())   # Print the result of the command.
